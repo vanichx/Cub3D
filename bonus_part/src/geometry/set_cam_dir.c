0001af361d0cf5a1886053c052abab965596df2e
vanichx <vanyapetrunin88@gmail.com>
2024-02-22 16:28:49 +0100

    Fixed EA, WE direction movements

--- bonus_part/src/geometry/set_cam_dir.c
+++ bonus_part/src/geometry/set_cam_dir.c
@@ -6,7 +6,7 @@
 /*   By: ipetruni <ipetruni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 14:29:57 by ipetruni          #+#    #+#             */
-/*   Updated: 2024/02/13 14:32:48 by ipetruni         ###   ########.fr       */
+/*   Updated: 2024/02/22 16:26:27 by ipetruni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	east(t_vec *front, t_vec *cam)
 	front->dir[X] = 1;
 	front->dir[Y] = 0;
 	cam->dir[X] = 0;
-	cam->dir[Y] = -0.66;
+	cam->dir[Y] = 0.66;
 }
 
 void	west(t_vec *front, t_vec *cam)
@@ -41,17 +41,17 @@ void	west(t_vec *front, t_vec *cam)
 	front->dir[X] = -1;
 	front->dir[Y] = 0;
 	cam->dir[X] = 0;
-	cam->dir[Y] = 0.66;
+	cam->dir[Y] = -0.66;
 }
 
 void	set_fr_cam_v(char player, t_vec *front, t_vec *cam)
 {
 	if (player == 'N')
 		north(front, cam);
-	else if (player == 'S')
+	if (player == 'S')
 		south(front, cam);
-	else if (player == 'E')
+	if (player == 'E')
 		east(front, cam);
-	else if (player == 'W')
+	if (player == 'W')
 		west(front, cam);
 }
